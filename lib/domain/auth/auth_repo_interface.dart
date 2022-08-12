7b2d875b1af89903d90adf1e383872a7673836e3
MohamedShahilK <shahilsinu007@gmail.com>
2022-08-12 17:20:51 +0530

    change void type of AuthRepoInterface to Either

--- lib/domain/auth/auth_repo_interface.dart
+++ lib/domain/auth/auth_repo_interface.dart
@@ -1,15 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:domain_driven_development_arch/domain/auth/core/failures/authFailures/auth_failures.dart';
 import 'package:domain_driven_development_arch/domain/auth/valueobjects.dart';
 
 abstract class AuthRepoInterface {
-  Future<void> registerWithEmailAndPassword({
+  Future<Either<AuthFailures, Unit>> registerWithEmailAndPassword({
     required EmailAddress emailAddress,
     required Password password,
   });
 
-  Future<void> signInWithEmailAndPassword({
+  Future<Either<AuthFailures, Unit>> signInWithEmailAndPassword({
     required EmailAddress emailAddress,
     required Password password,
   });
 
-  Future<void> signInWithGoogle();
+  Future<Either<AuthFailures, Unit>> signInWithGoogle();
 }
+
+//Dart's "void" is only a keyword, not a type, and it cannot be used as a generic type parameter.
+//But "dartz package provides us a type" called "Unit(unit)", which we can be usd as a "functional
+//equivalent" to "Dart's dumb "void"" keyword.
