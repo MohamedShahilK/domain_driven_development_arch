ee1a8321b12250f9376eb783823460e2e44e1164
MohamedShahilK <shahilsinu007@gmail.com>
2022-08-12 12:00:09 +0530

    validation and sample exception in emailAdd entity

--- lib/domain/auth/email_address_entity.dart
+++ lib/domain/auth/email_address_entity.dart
@@ -1,9 +1,20 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 class EmailAddress {
   final String value;
 
-  // EmailAddress(this.value) : assert(value != null);
-  EmailAddress(this.value);
+  //The most straightforward way of validating at instantiation is to create a "factory constructor"
+  //which will "perform the validation logic by throwing Exceptions if something doesn't play right"
+  //and "then finally instantiate an EmailAddress by calling a private constructor."
+
+  factory EmailAddress(String input) {
+    // assert(input != null);
+    return EmailAddress._(
+      //which returns the input(as String) only if it is validated.Otherwise throw an exception
+      validateEmailAddress(input),
+    );
+  }
+
+  //private constructor
+  const EmailAddress._(this.value);
 
   @override
   String toString() => 'EmailAddress($value)';
@@ -18,4 +29,23 @@ class EmailAddress {
 
   @override
   int get hashCode => value.hashCode;
+}
+
+//Sample Exception
+class InvalidEmailException implements Exception {
+  final String fieldValue;
+
+  InvalidEmailException({required this.fieldValue});
+}
+
+//Validation
+String validateEmailAddress(String input) {
+  // Maybe not the most robust way of email validation but it's good enough
+  const emailRegex =
+      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return input;
+  } else {
+    throw InvalidEmailException(fieldValue: input);
+  }
 }
