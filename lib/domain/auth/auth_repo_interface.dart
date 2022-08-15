83a8bb5f2ce03f6116e95ebb16fa3cb9d76cb0ce
MohamedShahilK <shahilsinu007@gmail.com>
2022-08-15 16:57:05 +0530

    auth interface updated

--- lib/domain/auth/auth_repo_interface.dart
+++ lib/domain/auth/auth_repo_interface.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:domain_driven_development_arch/domain/auth/core/failures_and_errors/authFailures/auth_failures.dart';
+import 'package:domain_driven_development_arch/domain/auth/each_user.dart';
 import 'package:domain_driven_development_arch/domain/auth/valueobjects.dart';
 
-
 abstract class AuthRepoInterface {
+  Future<Option<User>> getSignedUserByAuthChecking();
+
   Future<Either<AuthFailures, Unit>> registerWithEmailAndPassword({
     required EmailAddress emailAddress,
     required Password password,
@@ -15,6 +17,8 @@ abstract class AuthRepoInterface {
   });
 
   Future<Either<AuthFailures, Unit>> signInWithGoogle();
+
+  Future<void> signOut();
 }
 
 //Dart's "void" is only a keyword, not a type, and it cannot be used as a generic type parameter.
