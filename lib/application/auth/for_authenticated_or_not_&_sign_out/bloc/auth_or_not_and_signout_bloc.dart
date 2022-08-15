3796084f72f0764a7c37f8b70e11d67a63f68610
MohamedShahilK <shahilsinu007@gmail.com>
2022-08-15 17:47:24 +0530

    auth_or_not_and_signout completed

--- lib/application/auth/for_authenticated_or_not_&_sign_out/bloc/auth_or_not_and_signout_bloc.dart
+++ lib/application/auth/for_authenticated_or_not_&_sign_out/bloc/auth_or_not_and_signout_bloc.dart
@@ -1,18 +1,32 @@
 // ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
+import 'package:domain_driven_development_arch/domain/auth/auth_repo_interface.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 
 part 'auth_or_not_and_signout_event.dart';
 part 'auth_or_not_and_signout_state.dart';
 part 'auth_or_not_and_signout_bloc.freezed.dart';
 
-class AuthOrNotAndSignoutBloc extends Bloc<AuthOrNotAndSignoutEvent, AuthOrNotAndSignoutState> {
-  AuthOrNotAndSignoutBloc() : super(const AuthOrNotAndSignoutState.initial()) {
-    on<AuthOrNotAndSignoutEvent>((event, emit) {
+@injectable
+class AuthOrNotAndSignoutBloc
+    extends Bloc<AuthOrNotAndSignoutEvent, AuthOrNotAndSignoutState> {
+  final AuthRepoInterface _authRepoInterface;
+  AuthOrNotAndSignoutBloc(this._authRepoInterface)
+      : super(const AuthOrNotAndSignoutState.initial()) {
+    on<AuthOrNotAndSignoutEvent>((event, emit) async {
       if (event is _AuthChecking) {
-        
+        final userFoundOrNot =
+            await _authRepoInterface.getSignedUserByAuthChecking();
+        emit(
+          userFoundOrNot.fold(
+            () => const AuthOrNotAndSignoutState.unauthenticated(),
+            (a) => const AuthOrNotAndSignoutState.authenticated(),
+          ),
+        );
       } else {
-        
+        await _authRepoInterface.signOut();
+        emit(const AuthOrNotAndSignoutState.unauthenticated());
       }
     });
   }
--- lib/core/Dependency Injection/dependency_injection.config.dart
+++ lib/core/Dependency Injection/dependency_injection.config.dart
@@ -5,16 +5,18 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:firebase_auth/firebase_auth.dart' as _i3;
+import 'package:firebase_auth/firebase_auth.dart' as _i4;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:google_sign_in/google_sign_in.dart' as _i4;
+import 'package:google_sign_in/google_sign_in.dart' as _i5;
 import 'package:injectable/injectable.dart' as _i2;
 
+import '../../application/auth/for_authenticated_or_not_&_sign_out/bloc/auth_or_not_and_signout_bloc.dart'
+    as _i3;
 import '../../application/auth/only_for_signin_and%20_register/bloc/signin_register_bloc.dart'
-    as _i7;
-import '../../domain/auth/auth_repo_interface.dart' as _i5;
-import '../../infrastructure/auth/auth_repo_impl.dart' as _i6;
-import 'injection_module.dart' as _i8; // ignore_for_file: unnecessary_lambdas
+    as _i8;
+import '../../domain/auth/auth_repo_interface.dart' as _i6;
+import '../../infrastructure/auth/auth_repo_impl.dart' as _i7;
+import 'injection_module.dart' as _i9; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -22,16 +24,17 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
   final injectionModuleForDependencies = _$InjectionModuleForDependencies();
-  gh.lazySingleton<_i3.FirebaseAuth>(
+  gh.factory<_i3.AuthOrNotAndSignoutBloc>(() => _i3.AuthOrNotAndSignoutBloc());
+  gh.lazySingleton<_i4.FirebaseAuth>(
       () => injectionModuleForDependencies.firebaseAuth);
-  gh.lazySingleton<_i4.GoogleSignIn>(
+  gh.lazySingleton<_i5.GoogleSignIn>(
       () => injectionModuleForDependencies.googleSignIn);
-  gh.lazySingleton<_i5.AuthRepoInterface>(
-      () => _i6.AuthRepoImpl(get<_i3.FirebaseAuth>(), get<_i4.GoogleSignIn>()));
-  gh.factory<_i7.SigninRegisterBloc>(
-      () => _i7.SigninRegisterBloc(get<_i5.AuthRepoInterface>()));
+  gh.lazySingleton<_i6.AuthRepoInterface>(
+      () => _i7.AuthRepoImpl(get<_i4.FirebaseAuth>(), get<_i5.GoogleSignIn>()));
+  gh.factory<_i8.SigninRegisterBloc>(
+      () => _i8.SigninRegisterBloc(get<_i6.AuthRepoInterface>()));
   return get;
 }
 
 class _$InjectionModuleForDependencies
-    extends _i8.InjectionModuleForDependencies {}
+    extends _i9.InjectionModuleForDependencies {}
